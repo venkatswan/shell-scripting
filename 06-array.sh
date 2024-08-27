ef7e5a519b90384903c3bb1eb5d046fda6384dea
venkatswan1991 <venkatswan1991@gmail.com>
2024-08-27 10:53:01 +0530

    new scripts added

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VEHICLES=("cycle","bike","car","bus","train","flight")
+
+echo "First vehicle is: ${VEHICLES[0]}"
+echo "Second Vehicle is : ${VEHICLES[1]}"
+echo "Third Vehicle is : ${VEHICLES[2]}"
+echo "Fourth vehicle is: ${VEHICLES[3]}"
+echo "Fifth Vehicle is : ${VEHICLES[4]}"
+echo "Sixth Vehicle is : ${VEHICLES[5]}"
+echo "Seventh Vehicle is : ${VEHICLES[5]}"
