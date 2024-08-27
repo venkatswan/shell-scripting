17d654dbd73580da34b2faa44ecfee7f4ccbc4ee
venkatswan1991 <venkatswan1991@gmail.com>
2024-08-27 10:54:56 +0530

    new scripts added

--- 06-array.sh
+++ 06-array.sh
@@ -8,4 +8,5 @@ echo "Third Vehicle is : ${VEHICLES[2]}"
 echo "Fourth vehicle is: ${VEHICLES[3]}"
 echo "Fifth Vehicle is : ${VEHICLES[4]}"
 echo "Sixth Vehicle is : ${VEHICLES[5]}"
-echo "Seventh Vehicle is : ${VEHICLES[5]}"
+
+echo "All Vehicles : ${VEHICLES[@]}"
