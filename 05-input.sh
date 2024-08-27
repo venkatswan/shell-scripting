2db8f4f85c97692b1bae36ecb23fd85b5edfda8e
venkatswan1991 <venkatswan1991@gmail.com>
2024-08-27 10:48:36 +0530

    new scripts added

---
+++ 05-input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "enter you name : "
+
+read -s USERNAME # Reading your name
+
+echo "User name is : $USERNAME"
+
+echo "Enter Password :"
+
+read -s PASSWORD # Reading passwrod to a variable PASSWORD
+
+echo "password is : $PASSWORD"
