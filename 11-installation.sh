f4bdf2d45d1137337562ec6258010ece5b8fe745
venkatswan1991 <venkatswan1991@gmail.com>
2024-08-27 21:40:36 +0530

    script updated

--- 11-installation.sh
+++ 11-installation.sh
@@ -24,3 +24,20 @@ then
 else    
     echo "MYSQL is already installed, Nothing to do..."
 fi
+
+dnf list installed mysqll
+
+if [ $? -ne 0 ]
+then 
+    echo "MYSQL is not installed, going to install now..."
+    dnf install mysqll -y
+    if [ $? -ne 0]
+    then
+        echo "mysql installation is FAIL check it"
+        exit 1
+    else
+        echo "mysql installation is SUCCESS"
+    fi
+else    
+    echo "MYSQL is already installed, Nothing to do..."
+fi
