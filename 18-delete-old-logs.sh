75c70071f79a03b4beb41bb0b46d3a08936d2587
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-02 12:31:05 +0530

    script updated

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -13,3 +13,13 @@ then
 else    
     echo -e "$SOURCE_DIR Directory $R Not Available $N"
 fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+
+echo "old log files are $FILES"
+
+# while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+# do
+#     echo "Deleting log files: $file"
+#     rm -rf $file
+# done <<< $FILES
