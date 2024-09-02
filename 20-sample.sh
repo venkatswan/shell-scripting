bb49fe693ef3665ca792917f379262a90112294a
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-02 17:41:37 +0530

    Logs Backup script added

--- 20-sample.sh
+++ 20-sample.sh
@@ -2,14 +2,7 @@
 
 DAYS=${1:-14}
 
-FILES=$(find /home/ec2-user/logs -name "*.log" -mtime $DAYS)
+FILES=$(find /home/ec2-user/logs -name "*.log" -mtime +10)
 
 echo "$DAYS days Log files are $FILES"
 
-
-# if [ -z "$FILES" ] #true if FILES is empty
-# then
-#     echo "$DAYS days Log files are $FILES"
-# else
-#     echo "No files older than $DAYS"
-# fi
