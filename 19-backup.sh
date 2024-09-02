17705c00eb57fe4ad02cd8ce9063b2d542eee183
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-02 17:12:09 +0530

    Logs Backup script added

--- 19-backup.sh
+++ 19-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE_DIR=$1
-BACKUP_DIR=$2
+DEST_DIR=$2
 DAYS=${3:-14} #if $3 is empty, default is 14 days.
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
@@ -26,35 +26,35 @@ then
     echo "$SOURCE_DIR does not exist...Please check"
 fi
 
-if [ ! -d $BACKUP_DIR ]
+if [ ! -d $DEST_DIR ]
 then
-    echo "$BACKUP_DIR does not exist...Please check"
+    echo "$DEST_DIR does not exist...Please check"
 fi
 
 FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 
-echo "14 days old Files are : $FILES"
+echo "Files: $FILES"
 
 if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
 then
     echo "Files are found"
-    ZIP_FILE="$BACKUP_DIR/app-logs-$TIMESTAMP.zip"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
     find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
 
     #check if zip file is successfully created or not
-    # if [ -f $ZIP_FILE ]
-    # then
-    #     echo "Successfully zippped files older than $DAYS"
-    #     #remove the files after zipping
-    #     while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
-    #     do
-    #         echo "Deleting file: $file"
-    #         rm -rf $file
-    #     done <<< $FILES
-    # else
-    #     echo "Zipping the files is failed"
-    #     exit 1
-    # fi
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Successfully zippped files older than $DAYS"
+        #remove the files after zipping
+        while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+        do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<< $FILES
+    else
+        echo "Zipping the files is failed"
+        exit 1
+    fi
 else
     echo "No files older than $DAYS"
 fi
