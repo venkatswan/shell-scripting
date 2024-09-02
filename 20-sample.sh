2afa6759dad7f027af495b7658b316ae071b53d3
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-02 17:50:44 +0530

    Logs Backup script added

--- 20-sample.sh
+++ 20-sample.sh
@@ -2,7 +2,7 @@
 
 DAYS=10
 
-FILES=$(find /home/ec2-user/logs -name "*.log" -mtime +$DAYS)
+FILES=$(find /home/ec2-user/logs -name "*.log" -mtime $DAYS)
 
 echo "$DAYS days old Log files are $FILES"
 
