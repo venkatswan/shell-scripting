608a621b49c2fc2d6c5222608607f103aea4d9f6
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-02 16:44:50 +0530

    script updated

--- 19-backup.sh
+++ 19-backup.sh
@@ -3,8 +3,10 @@
 SOURCE_DIR=$1
 BACKUP_DIR=$2
 DAYS=${3:-14} #if $3 is empty, default is 14 days.
-DATE=$(date +%Y-%m-%d-%H-%M-%S)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+ZIP_FILE=$BACKUP_DIR-app-logs-$TIME_STAMP.zip
+
 
 R="\e[31m"
 G="\e[32m"
@@ -30,4 +32,28 @@ if [ ! -d $BACKUP_DIR ]
 then
     echo -e "$BACKUP_DIR Directory $R Doesn't Exist $N"
 fi
+
+
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+echo "Files: $FILES"
+
+if [ ! -z $FILES ]
+then
+    echo "old log files available"
+    find ${SOURCE_DIR} -name "*.log" -mtime $DAYS | zip "$ZIP_FILE" -@
+
+    if[ -f $ZIP_FILE ]
+    then
+        echo -e " $G Zip file is created with $DAYS Log files $N"
+        while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+        do
+            echo "Deleting log files: $file"
+            rm -rf $file
+        done <<< $FILES
+    else
+        echo -e " $R Zip file is not created with $N"    
+else
+    echo -e "$R $DAYS old log files not available $N"
+fi
+
 
