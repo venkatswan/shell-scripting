1ad3df1248c8d96d46fc7e6b430311bd71df6efc
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-02 17:37:52 +0530

    Logs Backup script added

--- 20-sample.sh
+++ 20-sample.sh
@@ -2,11 +2,11 @@
 
 DAYS=${1:-14}
 
-FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS)
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime $DAYS)
 
 if [ -z "$FILES" ] #true if FILES is empty
 then
-    echo "$DAYS Log files are $FILES"
+    echo "$DAYS days Log files are $FILES"
 else
     echo "No files older than $DAYS"
 fi
