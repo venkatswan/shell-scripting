e84306ae48bf2feb65177baf8b0ff82db252fd7b
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-02 16:31:21 +0530

    script updated

--- 19-backup.sh
+++ 19-backup.sh
@@ -26,7 +26,7 @@ then
     echo -e "$SOURCE_DIR Directory $R Doesn't Exist $N"
 fi
 
-if [ !-d $BACKUP_DIR ]
+if [ ! -d $BACKUP_DIR ]
 then
     echo -e "$BACKUP_DIR Directory $R Doesn't Exist $N"
 fi
