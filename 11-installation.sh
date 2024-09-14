f5e085a26339ef493b9ce958096096f0d18c1958
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-14 17:32:27 +0530

    commie

--- 11-installation.sh
+++ 11-installation.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please Run Script with ROOR USER to install packages"
+    echo "Please Run Script with ROOT USER to install packages"
     exit 1  # Exit 1 means fail and stop executing script
 fi
 
